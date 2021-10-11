3cdbbbfa9a1ea1d8d761d41eda7f50bd266f8cd0
KEVIN TANUWIJAYA <kevintanuwijaya11@gmail.com>
2021-10-11 16:18:15 +0700

    initial folders

---
+++ src/programs/main.java
@@ -0,0 +1,15 @@
+package programs;
+
+import views.DoctorViews;
+
+public class main {
+
+	public main() {
+		new DoctorViews();
+	}
+
+	public static void main(String[] args) {
+		new main();
+	}
+
+}
---
+++ src/views/DoctorViews.java
@@ -0,0 +1,14 @@
+package views;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class DoctorViews extends JFrame{
+	
+	public DoctorViews() {
+		setSize(new Dimension(1000, 1000));
+		setVisible(true);
+	}
+
+}
