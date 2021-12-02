5a72fdc944d81b66d3d57e26a5e9d793df4798e8
KEVIN TANUWIJAYA <kevintanuwijaya11@gmail.com>
2021-12-02 17:11:56 +0700

    DatabaseConnection

--- src/utils/DatabaseConnection.java
+++ src/utils/DatabaseConnection.java
@@ -1,5 +1,30 @@
 package utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
 public class DatabaseConnection {
 
+	private static DatabaseConnection instance = null;
+	private Connection conn;
+	
+	public static DatabaseConnection getInstance() {
+		
+		if(instance == null) {
+			instance = new DatabaseConnection();
+		}
+		return instance;
+	}
+	
+	private DatabaseConnection() {
+		
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project_lab_ooad","root","");
+			System.out.println("Success");
+		} catch (Exception e) {
+			System.out.println("Failed");
+		}
+	}
+	
 }
