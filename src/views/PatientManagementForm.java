ef51606d87f55faf3ed8330501c4a264a8440622
KEVIN TANUWIJAYA <kevintanuwijaya11@gmail.com>
2021-12-22 17:19:21 +0700

    update back button to View

--- src/views/PatientManagementForm.java
+++ src/views/PatientManagementForm.java
@@ -10,10 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
 
@@ -276,8 +273,9 @@ public class PatientManagementForm extends JFrame{
 		backBtn.addActionListener(new ActionListener() {
 			
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
+			public void actionPerformed(ActionEvent e) {			
+				new MainMenu(employee);
+				dispose();
 				
 			}
 		});
